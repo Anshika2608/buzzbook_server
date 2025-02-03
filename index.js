658c23f80738d4cc0a43186f53ed5eb3e7afc6b4
Anshika2608 <anshika2210142@akgec.ac.in>
2025-02-03 18:02:51 +0530

    login_api_done

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express=require("express");
 const app=express();
 require("./config/config");
+const cookieParser=require("cookie-parser");
 const cors=require("cors");
 app.use(express.json());
+app.use(cookieParser());
 const port=process.env.PORT||3000;
 
 app.use("/",require("./Routes/AuthenticationRoute"))
-
+// app.use("/login",require("./Routes/AuthenticationRoute"))
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
