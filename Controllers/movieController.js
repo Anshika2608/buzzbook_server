00136635b2820b6add494ec67809ca1783fc6c45
Anshika2608 <anshika2210142@akgec.ac.in>
2025-02-04 19:30:57 +0530

    get_movie_api_created

--- Controllers/movieController.js
+++ Controllers/movieController.js
@@ -1,6 +1,12 @@
-const movies=require("../Models/movieModel")
+const movie=require("../Models/movieModel")
 const getMovie=async(req,res)=>{
-
+    try{
+       const listOfMovies=await movie.find({});
+       return res.status(201).json({message:"list of movies recieved successfully",listOfMovies})
+}catch(error){
+   return res.status(500).json({message:"error while getting list of movies",error:error.message})
+}
+   
 }
 const addMovie=async(req,res)=>{
 
