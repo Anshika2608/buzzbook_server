308766598c6d4efb5c3674e8f242d02be1918e59
Anshika2608 <anshika2210142@akgec.ac.in>
2025-02-04 21:35:04 +0530

    add_movie_api_created

--- Routes/MovieRoutes.js
+++ Routes/MovieRoutes.js
@@ -1,7 +1,11 @@
-const express=require("express")
-const router=express.Router()
-const{getMovie,addMovie}=require("../Controllers/movieController")
-router.get("/movie_list",getMovie);
- router.post("/add_movie",addMovie);
- 
-module.exports=router;
+const express = require("express");
+const router = express.Router();
+
+const {uploadMultiple} = require("../Middleware/Multer"); 
+const { getMovie, addMovie } = require("../Controllers/movieController");
+
+
+router.get("/movie_list", getMovie);
+router.post("/add_movie", uploadMultiple, addMovie);
+
+module.exports = router;
