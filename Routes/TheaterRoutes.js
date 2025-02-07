62425a82096c447733d7bdd4f1dee641e73da283
Anshika2608 <anshika2210142@akgec.ac.in>
2025-02-07 23:53:05 +0530

    get_theater_for_film_route_ created

--- Routes/TheaterRoutes.js
+++ Routes/TheaterRoutes.js
@@ -1,8 +1,9 @@
 const express=require("express");
-const { getTheater, addTheater,getSeatLayout } = require("../Controllers/theaterController");
+const { getTheater, addTheater,getSeatLayout, getTheaterForMovie } = require("../Controllers/theaterController");
 const generateSeatsMiddleware = require("../Middleware/SeatLayout");
 const router=express.Router();
 router.get("/theater_list",getTheater);
 router.post("/add_theater",generateSeatsMiddleware,addTheater);
 router.get("/seat_layout/:name",getSeatLayout)
+router.get("/get_theater/:location/:title",getTheaterForMovie)
 module.exports=router;
