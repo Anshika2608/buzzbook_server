503ea21e8f557c69c14c8e98d6715ceae9c3f03b
Anshika2608 <anshika2210142@akgec.ac.in>
2025-07-30 02:02:47 +0530

    update_Snacks_routes_updated

--- Routes/SnackRoutes.js
+++ Routes/SnackRoutes.js
@@ -5,6 +5,6 @@ const authenticate = require("../Middleware/Authenticate");
 const { uploadSnackImages } = require("../Middleware/Multer");
 router.get("/snack_list",authenticate,  getAllSnacks);
 router.post("/add_snack",authenticate,uploadSnackImages, addSnack);
-router.patch("/update_snack",  uploadSnackImages, updateSnack);
+router.patch("/update_snack",authenticate,  uploadSnackImages, updateSnack);
 router.delete("/delete_snack", authenticate, deleteSnack);  
 module.exports = router;
