f68867659333105c79dddedb76eb4a71aceb218a
Anshika Mittal <anshika2210142@akgec.ac.in>
2025-11-15 16:40:02 +0530

    updated_seat_held_logic

--- Routes/BookingRoutes.js
+++ Routes/BookingRoutes.js
@@ -5,7 +5,7 @@ const authenticate  = require("../Middleware/Authenticate");
 
 
 router.post("/hold-seats",authenticate, holdSeats);
-router.post("/temp/update", authenticate, updateTempBooking);
+router.put("/temp/update", authenticate, updateTempBooking);
 router.post("/temp/release", authenticate, releaseTempBooking);
 
 router.post("/book-seats",authenticate, confirmBooking);
